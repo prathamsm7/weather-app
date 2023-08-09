65bf169a8674386af29f955ee265362defe5a3c6
prathamsm7 <mungekarp8@gmail.com>
2023-08-09 19:14:14 +0530

    UI and Functionality completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,166 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { BiLoaderCircle } from "react-icons/bi";
+import { Toaster, toast } from "react-hot-toast";
+import { fetchData } from "./helperFunctions";
+import Weather from "./Weather";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [loading, setLoading] = useState({ type: "", status: false });
+  const [isDataAvailable, setIsDataAvailable] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  //Get Weather data by User Device location
+  const getLocation = () => {
+    setLoading({ status: true, type: "button" });
+    setIsDataAvailable(false);
+    setSearchText("");
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const data = await fetchData(
+            "",
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setWeatherData(data);
+
+          if (!data.base) {
+            toast.error("Something went wrong");
+            setLoading({ type: "", status: false });
+            return false;
+          }
+
+          setTimeout(() => {
+            setLoading({ type: "", status: false });
+            setIsDataAvailable((prev) => !prev);
+          }, 1000);
+        },
+        (error) => {
+          console.error(error);
+          setLoading({ status: false, type: "" });
+          toast.error("Your precise location could not be determined.");
+        }
+      );
+    } else {
+      // console.error("Geolocation is not supported by this browser.");
+      setLoading({ status: false, type: "" });
+      toast.error("Geolocation is not supported by this browser.");
+      return false;
+    }
+  };
+
+  function handleChange(e) {
+    const searchText = e.target.value;
+    let setTypingTimeout;
+
+    const re = /^[A-Za-z]+$/;
+    if (searchText === "" || re.test(searchText)) {
+      setSearchText(searchText);
+    }
+
+    setLoading({
+      type: "input",
+      status: true,
+    });
+
+    setTypingTimeout = setTimeout(() => {
+      console.log("User stopped typing:");
+      // Perform actions when user stops typing
+      setLoading({
+        type: "",
+        status: false,
+      });
+
+      clearTimeout(setTypingTimeout);
+    }, 1000);
+  }
+
+  //Get Data on form submit
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (searchText.length < 3) {
+      toast.error("Enter atleast 3 or more letters", {
+        style: { backgroundColor: "orange", color: "white" },
+        icon: "ℹ",
+      });
+      return;
+    }
+    getData();
+  }
+
+  async function getData() {
+    const data = await fetchData(searchText);
+    if (data.cod === "404") {
+      toast.error(data.message || "Something went wrong");
+      setLoading({ type: "", status: false });
+      return false;
+    }
+    setWeatherData(data);
+    setIsDataAvailable((prev) => !prev);
+    setLoading({ type: "", status: false });
+    setSearchText("");
+  }
+
+  //Get Data on form input change
+  useEffect(() => {
+    const timeoutId =
+      searchText.length >= 3 &&
+      setTimeout(() => {
+        getData();
+      }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchText]);
 
   return (
     <>
-      <h1>Weather App</h1>
+      <Toaster />
+      <div className="appContainer">
+        <div className="head">
+          {isDataAvailable && (
+            <IoMdArrowRoundBack
+              onClick={() => {
+                setIsDataAvailable((prev) => !prev);
+                setSearchText("");
+              }}
+            />
+          )}
+          <p>Weather App</p>
+        </div>
+        <hr />
+        <div>
+          {isDataAvailable ? (
+            <Weather weatherData={weatherData} />
+          ) : (
+            <>
+              <form action="" onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  value={searchText}
+                  placeholder="Enter City Name"
+                  onChange={handleChange}
+                />
+                {loading.status && loading.type === "input" && (
+                  <BiLoaderCircle className="spinner" />
+                )}
+                <div className="divider">or</div>
+              </form>
+              <div className="buttonDiv">
+                <button onClick={getLocation}>Get Device Location</button>
+                {loading.status && loading.type === "button" && (
+                  <BiLoaderCircle className="spinner" />
+                )}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/helperFunctions.js
@@ -0,0 +1,21 @@
+const api = `https://api.openweathermap.org/data/2.5/weather?`
+const apiId = import.meta.env.VITE_APIKEY
+
+export const imageApi = `http://openweathermap.org/img/wn/`
+
+export async function fetchData(text,lat,lng){
+    const apiURL = text ? `${api}q=${text}&appid=${apiId}&units=metric` : `${api}lat=${lat}&lon=${lng}&appid=${apiId}&units=metric`
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        return data
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+        return error
+      }
+}
+
+export  function getCountryName(id) {
+    let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+    return regionNames.of(id);
+  }
